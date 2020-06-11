94172b4c2450f9a3e1261e9c759c1d8421531ef7
Nikola Vukobrat <nikola.vukobrat@gmail.com>
2020-06-11 11:30:02 +0200

    PyTextRank example

---
+++ third-party-test/extractive-graph-base-textrank-pytextrank.py
@@ -0,0 +1,16 @@
+import pytextrank
+import spacy
+
+from exnihilo import data
+
+text = data.read_from_file("../input.txt")
+
+nlp = spacy.load("en_core_web_sm")
+tr = pytextrank.TextRank()
+tr.load_stopwords(path="stop.json")
+nlp.add_pipe(tr.PipelineComponent, name="textrank", last=True)
+doc = nlp(text)
+
+print("Summary:")
+for sent in doc._.textrank.summary(limit_phrases=15, limit_sentences=2):
+    print(sent)
